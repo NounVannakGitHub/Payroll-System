a2ef8c44bf85659040795590ffcaa413c71db30f
NounVannakGitHub <nounvannakrootid@gmail.com>
2015-09-09 23:54:56 +0700

    update ILicense Interface and LicenseImpl class

--- LicenseImpl.vb
+++ LicenseImpl.vb
@@ -5,11 +5,9 @@ Public Class LicenseImpl
 
     Public Function IsLicensed(ByVal licenseName As String, ByVal licenseKey As String) As Boolean Implements ILicense.IsLicensed
         Dim isLicense = False
-        Dim licName As String = GetLicense.Item(0)
-        Dim licKey As String = GetLicense.Item(1)
-        If licenseKey.Equals(licKey) And licenseName.Equals(licName) Then
+        If licenseKey.Equals(GetLicense.LicenseKey) And licenseName.Equals(GetLicense.LicenseName) Then
             isLicense = True
-        ElseIf licenseKey.Equals(licKey) Then
+        ElseIf licenseKey.Equals(GetLicense.LicenseKey) Then
             isLicense = True
         Else
             isLicense = False
@@ -29,9 +27,9 @@ Public Class LicenseImpl
         fileStream.Close()
     End Sub
 
-    Public Function GetLicense() As List(Of String) Implements ILicense.GetLicense
+    Public Function GetLicense() As License Implements ILicense.GetLicense
         Dim fileSrc As String = Application.StartupPath & "License.bin"
-        Dim obj As New List(Of String)
+        Dim obj As New License
         If FileIO.FileSystem.FileExists(fileSrc) Then
             Dim fileStream As FileStream = New FileStream(fileSrc, FileMode.Open)
             Dim binaryFormat As New BinaryFormatter
