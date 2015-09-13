741dd6b6415d9809c85c643e905cf9483ffdebcc
NounVannakGitHub <nounvannakrootid@gmail.com>
2015-09-13 18:37:51 +0700

    full fixed not error

--- frmBenefitAndDeductionType.vb
+++ frmBenefitAndDeductionType.vb
@@ -7,20 +7,4 @@
     Private Sub btnClosePanel_Click(sender As Object, e As EventArgs) Handles btnClosePanel.Click
         Me.Close()
     End Sub
-
-    Private Sub tbtnNew_Click(sender As Object, e As EventArgs) Handles tbtnNew.Click
-        frmBenefitType.ShowDialog()
-    End Sub
-
-    Private Sub tbtnEdit_Click(sender As Object, e As EventArgs) Handles tbtnEdit.Click
-        frmEditBenefitType.ShowDialog()
-    End Sub
-
-    Private Sub tbtnNewDeductionType_Click(sender As Object, e As EventArgs) Handles tbtnNewDeductionType.Click
-        frmAddDeductionType.ShowDialog()
-    End Sub
-
-    Private Sub tbtnEditDeductionType_Click(sender As Object, e As EventArgs) Handles tbtnEditDeductionType.Click
-        frmEditDeductionType.ShowDialog()
-    End Sub
 End Class
